a22d9fcb39e19d260e449723928f70bb6909b4f4
JYEJYE <dldks1212@gmail.com>
2021-06-14 10:57:35 +0900

    JS: Navbar Menu Click

--- js/main.js
+++ js/main.js
@@ -4,11 +4,23 @@
 const navBar = document.querySelector('#navbar');
 const navBarHeight = navBar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
-  console.log(navBarHeight);
   if(window.scrollY > navBarHeight) {
     navBar.classList.add('navbar--darker');
   } else {
     navBar.classList.remove('navbar--darker');
   }
 });
+
+
+// Handle scrolling  when the navbar menu is clicked
+const navBarMenu = document.querySelector('.navbar__menu');
+navBarMenu.addEventListener('click', (event) => {  
+  const target = event.target;
+  const link = target.dataset.link;
+  if(link == null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+})
+
